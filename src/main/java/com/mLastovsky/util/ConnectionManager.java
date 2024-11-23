2865f10de657dc00dacfb7cce4debaef934cd27a
mLastovsky <LastovskyTech@gmail.com>
2024-11-23 15:42:37 +0300

    Refactor: ConnectionManager now uses connection pooling

--- src/main/java/com/mLastovsky/util/ConnectionManager.java
+++ src/main/java/com/mLastovsky/util/ConnectionManager.java
@@ -3,18 +3,65 @@ package com.mLastovsky.util;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 
+import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 @UtilityClass
 public class ConnectionManager {
 
-    public static final String URL_KEY = "db.url";
-    public static final String USERNAME_KEY = "db.username";
-    public static final String PASSWORD_KEY = "db.password";
+    private static final String URL_KEY = "db.url";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
+    private static final String POOL_SIZE_KEY = "db.pool.size";
+    private static final Integer DEFAULT_POOL_SIZE = 10;
+    private static BlockingQueue<Connection> pool;
+    private static List<Connection> sourceConnections;
 
     static {
         loadDriver();
+        initConnectionPool();
+    }
+
+    public static Connection get() {
+        try {
+            return pool.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @SneakyThrows
+    public static void closePool() {
+        for (Connection sourceConnection : sourceConnections) {
+            sourceConnection.close();
+        }
+    }
+
+    @SneakyThrows
+    private static void loadDriver() {
+        Class.forName("org.postgresql.Driver");
+    }
+
+    private static void initConnectionPool() {
+        var poolSize = PropertiesUtil.get(POOL_SIZE_KEY);
+        var size = poolSize == null ? DEFAULT_POOL_SIZE : Integer.parseInt(poolSize);
+        pool = new ArrayBlockingQueue<>(size);
+        sourceConnections = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            var connection = open();
+            var proxyConnection = (Connection) Proxy.newProxyInstance(ConnectionManager.class.getClassLoader(), new Class[]{Connection.class},
+                    (proxy, method, args) -> method.getName().equals("close")
+                            ? pool.add((Connection) proxy)
+                            : method.invoke(connection, args)
+            );
+            pool.add(proxyConnection);
+            sourceConnections.add(connection);
+        }
     }
 
     @SneakyThrows
@@ -24,10 +71,5 @@ public class ConnectionManager {
                 PropertiesUtil.get(USERNAME_KEY),
                 PropertiesUtil.get(PASSWORD_KEY)
         );
-    }
-
-    @SneakyThrows
-    private static void loadDriver() {
-        Class.forName("org.postgresql.Driver");
     }
 }
