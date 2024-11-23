7c7d6bff568307e7f38a2ad97d6b5c927345e9d5
mLastovsky <LastovskyTech@gmail.com>
2024-11-23 15:06:14 +0300

    add: user entity

---
+++ src/main/java/com/mLastovsky/entity/User.java
@@ -0,0 +1,14 @@
+package com.mLastovsky.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class User extends Entity{
+
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+}
