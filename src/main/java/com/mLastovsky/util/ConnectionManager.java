5e5b9fabd24fe4e4ad189ef6263d92e530f0b9cf
mLastovsky <LastovskyTech@gmail.com>
2024-11-23 15:29:28 +0300

    Add: ConnectionManager utility class for database connections

---
+++ src/main/java/com/mLastovsky/util/ConnectionManager.java
@@ -0,0 +1,33 @@
+package com.mLastovsky.util;
+
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@UtilityClass
+public class ConnectionManager {
+
+    public static final String URL_KEY = "db.url";
+    public static final String USERNAME_KEY = "db.username";
+    public static final String PASSWORD_KEY = "db.password";
+
+    static {
+        loadDriver();
+    }
+
+    @SneakyThrows
+    public static Connection open() {
+        return DriverManager.getConnection(
+                PropertiesUtil.get(URL_KEY),
+                PropertiesUtil.get(USERNAME_KEY),
+                PropertiesUtil.get(PASSWORD_KEY)
+        );
+    }
+
+    @SneakyThrows
+    private static void loadDriver() {
+        Class.forName("org.postgresql.Driver");
+    }
+}
