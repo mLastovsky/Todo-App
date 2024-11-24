3de87cf7e33859b23d3c084e7c3090240fcf1559
mLastovsky <LastovskyTech@gmail.com>
2024-11-24 19:19:21 +0300

    Feat: add validationResult class

---
+++ src/main/java/com/mLastovsky/validator/ValidationResult.java
@@ -0,0 +1,20 @@
+package com.mLastovsky.validator;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ValidationResult {
+
+    private final List<Error> errors = new ArrayList<>();
+
+    public void add(Error error){
+        this.errors.add(error);
+    }
+
+    public boolean isValid(){
+        return errors.isEmpty();
+    }
+}
