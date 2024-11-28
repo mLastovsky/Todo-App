d9ae1383bd5fad705461b232d7ac4764d7eb5e34
mLastovsky <LastovskyTech@gmail.com>
2024-11-28 15:02:56 +0300

    refactor: Centralize URL paths in UrlPath utility class

---
+++ src/main/java/com/mLastovsky/util/UrlPath.java
@@ -0,0 +1,12 @@
+package com.mLastovsky.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UrlPath {
+
+    public static final String LOGIN = "/login";
+    public static final String REGISTRATION = "/registration";
+    public static final String TODOS = "/todos";
+    public static final String LOGOUT = "/logout";
+}
