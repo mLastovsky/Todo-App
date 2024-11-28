ecf587fe0ef97cf2345131232927ac234dd9f953
mLastovsky <LastovskyTech@gmail.com>
2024-11-28 15:05:29 +0300

    feat: Implement authorization filter

---
+++ src/main/java/com/mLastovsky/filter/AuthorizationFilter.java
@@ -0,0 +1,38 @@
+package com.mLastovsky.filter;
+
+import com.mLastovsky.dto.UserDto;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static com.mLastovsky.util.UrlPath.*;
+
+@WebFilter("/*")
+public class AuthorizationFilter implements Filter {
+
+    private static final Set<String> PUBLIC_PATH = Set.of(LOGIN, REGISTRATION);
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        var uri = ((HttpServletRequest) servletRequest).getRequestURI();
+        if(isPublicPath(uri) || isUserLoggedIn(servletRequest)){
+            filterChain.doFilter(servletRequest,servletResponse);
+        } else{
+            ((HttpServletResponse) servletResponse).sendRedirect(LOGIN);
+        }
+    }
+
+    private boolean isPublicPath(String uri) {
+        return PUBLIC_PATH.stream().anyMatch(uri::startsWith);
+    }
+
+    private boolean isUserLoggedIn(ServletRequest servletRequest) {
+        var user = (UserDto) ((HttpServletRequest) servletRequest).getSession().getAttribute("user");
+
+        return user != null;
+    }
+}
