840d944e786d9697d102aac1ebac455a12d047cc
mLastovsky <LastovskyTech@gmail.com>
2024-11-29 21:16:11 +0300

    add: create CreateTodoDto

---
+++ src/main/java/com/mLastovsky/dto/CreateTodoDto.java
@@ -0,0 +1,13 @@
+package com.mLastovsky.dto;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Builder
+@Value
+public class CreateTodoDto {
+
+    Long userId;
+    String task;
+    Boolean completed;
+}
