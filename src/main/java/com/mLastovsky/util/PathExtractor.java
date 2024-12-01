09430ac6c8c0195a6d7b325e00b2c35c9cbde155
mLastovsky <LastovskyTech@gmail.com>
2024-12-01 03:10:42 +0300

    refactor: remove 'magic numbers'

--- src/main/java/com/mLastovsky/util/PathExtractor.java
+++ src/main/java/com/mLastovsky/util/PathExtractor.java
@@ -4,12 +4,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
-public class PathIdExtractor {
+public class PathExtractor {
+
+    private static final int ID_INDEX = 1;
+    private static final String PATH_SEPARATOR = "/";
 
     public static Long getIdFromPath(HttpServletRequest req){
-        var pathInfo = req.getPathInfo();
-        var parts = pathInfo.split("/");
-        var id = parts[1];
+        var params = req.getPathInfo().split(PATH_SEPARATOR);
+        var id = params[ID_INDEX];
 
         return Long.parseLong(id);
     }
