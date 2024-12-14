eaac5a4f570aaa21f21570f05a4e31c517524fe1
mLastovsky <LastovskyTech@gmail.com>
2024-12-14 13:26:03 +0300

    feat (logging): add detailed logging to TodoDao for better traceability

--- src/main/java/com/mLastovsky/dao/TodoDao.java
+++ src/main/java/com/mLastovsky/dao/TodoDao.java
@@ -3,6 +3,7 @@ package com.mLastovsky.dao;
 import com.mLastovsky.entity.TodoEntity;
 import com.mLastovsky.exception.DaoException;
 import com.mLastovsky.util.ConnectionManager;
+import lombok.extern.slf4j.Slf4j;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 public class TodoDao implements Dao<Long, TodoEntity> {
 
     private static final TodoDao INSTANCE = new TodoDao();
@@ -54,18 +56,28 @@ public class TodoDao implements Dao<Long, TodoEntity> {
 
     @Override
     public boolean delete(Long id) {
+        log.debug("Attempting to delete todo with id: {}", id);
         try (var connection = ConnectionManager.get();
              var preparedStatement = connection.prepareStatement(SQL_DELETE)) {
             preparedStatement.setLong(1, id);
 
-            return preparedStatement.executeUpdate() > 0;
+            boolean isDeleted = preparedStatement.executeUpdate() > 0;
+            if (isDeleted) {
+                log.info("Todo with id: {} successfully deleted", id);
+            } else {
+                log.warn("Todo with id: {} not found for deletion", id);
+            }
+
+            return isDeleted;
         } catch (SQLException e) {
+            log.error("Error while deleting todo with id: {}", id, e);
             throw new DaoException(e);
         }
     }
 
     @Override
     public TodoEntity save(TodoEntity todo) {
+        log.debug("Attempting to save new todo for userId: {}", todo.getUserId());
         try (var connection = ConnectionManager.get();
              var preparedStatement = connection.prepareStatement(SQL_SAVE, Statement.RETURN_GENERATED_KEYS)) {
             preparedStatement.setLong(1, todo.getUserId());
@@ -76,16 +88,21 @@ public class TodoDao implements Dao<Long, TodoEntity> {
             var generatedKeys = preparedStatement.getGeneratedKeys();
             if (generatedKeys.next()) {
                 todo.setId(generatedKeys.getLong("id"));
+                log.info("Todo saved successfully with generated id: {}", todo.getId());
+            } else {
+                log.warn("Failed to retrieve generated id for todo");
             }
 
             return todo;
         } catch (SQLException e) {
+            log.error("Error while saving todo for userId: {}", todo.getUserId(), e);
             throw new DaoException(e);
         }
     }
 
     @Override
     public void update(TodoEntity todo) {
+        log.debug("Attempting to update todo with id: {}", todo.getId());
         try (var connection = ConnectionManager.get();
              var preparedStatement = connection.prepareStatement(SQL_UPDATE)) {
             preparedStatement.setLong(1, todo.getUserId());
@@ -93,32 +110,44 @@ public class TodoDao implements Dao<Long, TodoEntity> {
             preparedStatement.setBoolean(3, todo.getCompleted());
             preparedStatement.setLong(4, todo.getId());
 
-            preparedStatement.executeUpdate();
+            int rowsUpdated = preparedStatement.executeUpdate();
+            if (rowsUpdated > 0) {
+                log.info("Todo with id: {} successfully updated", todo.getId());
+            } else {
+                log.warn("Todo with id: {} not found for update", todo.getId());
+            }
         } catch (SQLException e) {
+            log.error("Error while updating todo with id: {}", todo.getId(), e);
             throw new DaoException(e);
         }
     }
 
     @Override
     public Optional<TodoEntity> findById(Long id) {
+        log.debug("Attempting to find todo by id: {}", id);
         try (var connection = ConnectionManager.get();
              var preparedStatement = connection.prepareStatement(SQL_FIND_BY_ID)) {
             preparedStatement.setLong(1, id);
 
             var resultSet = preparedStatement.executeQuery();
             TodoEntity todo = null;
-            while (resultSet.next()) {
+            if (resultSet.next()) {
                 todo = buildTodo(resultSet);
+                log.info("Todo with id: {} found", id);
+            } else {
+                log.warn("Todo with id: {} not found", id);
             }
 
             return Optional.ofNullable(todo);
         } catch (SQLException e) {
+            log.error("Error while finding todo by id: {}", id, e);
             throw new DaoException(e);
         }
     }
 
     @Override
     public List<TodoEntity> findAll() {
+        log.debug("Attempting to find all todos");
         try (var connection = ConnectionManager.get();
              var preparedStatement = connection.prepareStatement(SQL_FIND_ALL)) {
 
@@ -128,13 +157,16 @@ public class TodoDao implements Dao<Long, TodoEntity> {
                 todos.add(buildTodo(resultSet));
             }
 
+            log.info("Total todos found: {}", todos.size());
             return todos;
         } catch (SQLException e) {
+            log.error("Error while finding all todos", e);
             throw new DaoException(e);
         }
     }
 
-    public List<TodoEntity> findAllUserTodos(Long userId){
+    public List<TodoEntity> findAllUserTodos(Long userId) {
+        log.debug("Attempting to find all todos for userId: {}", userId);
         try (var connection = ConnectionManager.get();
              var preparedStatement = connection.prepareStatement(SQL_FIND_ALL_USER_TODOS)) {
             preparedStatement.setLong(1, userId);
@@ -145,13 +177,16 @@ public class TodoDao implements Dao<Long, TodoEntity> {
                 todos.add(buildTodo(resultSet));
             }
 
+            log.info("Total todos found for userId {}: {}", userId, todos.size());
             return todos;
         } catch (SQLException e) {
+            log.error("Error while finding todos for userId: {}", userId, e);
             throw new DaoException(e);
         }
     }
 
     private static TodoEntity buildTodo(ResultSet resultSet) throws SQLException {
+        log.trace("Building TodoEntity from ResultSet");
         return TodoEntity.builder()
                 .id(resultSet.getLong("id"))
                 .userId(resultSet.getLong("user_id"))
